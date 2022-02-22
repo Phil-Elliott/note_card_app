87fe6d4f8d142982c40f2728f905d013fd98ffe5
Phil <phillippelliott@gmail.com>
2022-02-21 19:08:57 -0500

    cleaned up code

--- src/Content/Add/Components/CreateCards/Components/Card-container-left.tsx
+++ src/Content/Add/Components/CreateCards/Components/Card-container-left.tsx
@@ -1,29 +1,43 @@
 import React from "react"
 
-const CardContainerLeft = () => {
+const CardContainerLeft = ({
+  inputData,
+  changeInputData,
+}: {
+  inputData: any
+  changeInputData: any
+}) => {
   return (
-    <div className="input-add-card-container">
-      <div className="input-add-card">
-        <p>Question</p>
-        <textarea
-          maxLength={150}
-          placeholder="Awesome note cards"
-          value={inputData.question}
-          onChange={(e) =>
-            setInputData({ ...inputData, question: e.target.value })
-          }
-        />
+    <div className="add-card-left">
+      <div className="input-add-card-container">
+        <div className="input-add-card">
+          <p>Question</p>
+          <textarea
+            maxLength={150}
+            placeholder="Awesome note cards"
+            value={inputData.question}
+            onChange={(e) =>
+              changeInputData({ ...inputData, question: e.target.value })
+            }
+          />
+        </div>
+        <div className="input-add-card">
+          <p>Answer</p>
+          <textarea
+            maxLength={150}
+            placeholder="Science"
+            value={inputData.answer}
+            onChange={(e) =>
+              changeInputData({ ...inputData, answer: e.target.value })
+            }
+          />
+        </div>
       </div>
-      <div className="input-add-card">
-        <p>Answer</p>
-        <textarea
-          maxLength={150}
-          placeholder="Science"
-          value={inputData.answer}
-          onChange={(e) =>
-            setInputData({ ...inputData, answer: e.target.value })
-          }
-        />
+      <div>
+        <div className="add-card-bttns">
+          <button className="add-card-bttn-left">Back</button>
+          <button className="add-card-bttn-right">Add</button>
+        </div>
       </div>
     </div>
   )
--- src/Content/Add/Components/CreateCards/CreateCards.tsx
+++ src/Content/Add/Components/CreateCards/CreateCards.tsx
@@ -1,86 +1,27 @@
 import React, { useState } from "react"
+import CardContainerLeft from "./Components/Card-container-left"
+import CardContainerRight from "./Components/Card-container-right"
 import "./CreateCards.scss"
-import {
-  FaChevronLeft,
-  FaChevronRight,
-  FaArrowAltCircleRight,
-  FaArrowAltCircleLeft,
-} from "react-icons/fa"
 
-interface titleData {
+interface cardData {
   question: string
   answer: string
 }
 
 const CreateCards = () => {
-  const [inputData, setInputData] = useState<Partial<titleData>>({})
-  const [flipCard, setFlipCard] = useState<boolean>(false)
+  const [inputData, setInputData] = useState<Partial<cardData>>({})
+
+  const changeInputData = (e: object) => {
+    setInputData(e)
+  }
 
   return (
     <div className="card-add-container">
-      <div className="add-card-left">
-        <div className="input-add-card-container">
-          <div className="input-add-card">
-            <p>Question</p>
-            <textarea
-              maxLength={150}
-              placeholder="Awesome note cards"
-              value={inputData.question}
-              onChange={(e) =>
-                setInputData({ ...inputData, question: e.target.value })
-              }
-            />
-          </div>
-          <div className="input-add-card">
-            <p>Answer</p>
-            <textarea
-              maxLength={150}
-              placeholder="Science"
-              value={inputData.answer}
-              onChange={(e) =>
-                setInputData({ ...inputData, answer: e.target.value })
-              }
-            />
-          </div>
-        </div>
-        <div>
-          <div className="add-card-bttns">
-            <button className="add-card-bttn-left">Back</button>
-            <button className="add-card-bttn-right">Add</button>
-          </div>
-        </div>
-      </div>
-      <div className="add-card-right-main">
-        <FaChevronLeft className="add-card-arrow" />
-        <div className="add-card-right">
-          <div
-            className="card-side-container"
-            style={
-              flipCard
-                ? { transform: "rotateY(180deg)" }
-                : { transform: "rotateY(0deg)" }
-            }
-          >
-            <div className="add-card-right-front">
-              <h1>Question 1</h1>
-              <h2>{inputData.question}</h2>
-              <FaArrowAltCircleRight
-                className="card-flip-icon"
-                onClick={() => setFlipCard(!flipCard)}
-              />
-            </div>
-            <div className="add-card-right-back">
-              <h1>Answer</h1>
-              <h2>{inputData.answer}</h2>
-              <FaArrowAltCircleLeft
-                className="card-flip-icon"
-                onClick={() => setFlipCard(!flipCard)}
-              />
-            </div>
-          </div>
-        </div>
-        <FaChevronRight className="add-card-arrow" />
-      </div>
+      <CardContainerLeft
+        inputData={inputData}
+        changeInputData={changeInputData}
+      />
+      <CardContainerRight inputData={inputData} />
     </div>
   )
 }
